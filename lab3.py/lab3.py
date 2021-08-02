0e3ab877dbe406f2a54c6f537f8b1e009ed17072
Danib0y97 <66141645+Danib0y97@users.noreply.github.com>
2021-08-02 20:01:35 -0300

    lab3
    Tudo funcionando 100% :)

--- lab3.py/lab3.py
+++ lab3.py/lab3.py
@@ -12,7 +12,7 @@ class VeiculoAutomotor():
 class Automovel(VeiculoAutomotor):
     def __init__(self, dono:str , placa:str , combustivel:str, lugares:int, portas:int, ano:int):     
       """Construindo um construtor com os objetos que um Automovel possui."""
-      super.__init__( dono, placa, combustivel)
+      super().__init__( dono, placa, combustivel)
       self.lugares = lugares 
       self.portas = portas 
       self.ano = ano  
@@ -29,11 +29,11 @@ class Automovel(VeiculoAutomotor):
 class Caminhao(VeiculoAutomotor):
     def __init__(self, dono:str , placa:str , combustivel:str, cargaMax:int):
      """Construindo um construtor com os objetos que um Caminhão possui"""
-     super.__init__( dono, placa, combustivel)
+     super().__init__( dono, placa, combustivel)
      self.cargaMax = cargaMax  
 #b)
     def __str__(self:object) -> (str):
-       return "Dono: {}, placa: {}, combustível: {}, carga máxima: : {} toneladas".format(self.dono, self.placa,self.combustivel, self.cargaMax)
+       return "Dono: {}, placa: {}, combustível: {}, carga máxima: {} toneladas".format(self.dono, self.placa,self.combustivel, self.cargaMax)
 
 
 
