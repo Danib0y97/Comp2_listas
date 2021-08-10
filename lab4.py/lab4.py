0066be6c7aa0a4a62c0e4fd9c31d5c81f0105543
Danib0y97 <66141645+Danib0y97@users.noreply.github.com>
2021-08-10 20:15:31 -0300

    lab 4
    Questão 1 feita e pronta

--- lab4.py/lab4.py
+++ lab4.py/lab4.py
@@ -1,5 +1,36 @@
-def contatos():
-    dicionario1 = {}
-    dicionario2 = {}
-    return dicionario1.update(dicionario2)
- #rever   
+#Daniel Ferreira 
+#1)
+def contatos(lista:list) -> dict:
+    """Recebe uma lista de dicionários, retornando um dicionário."""    
+    nomes = []
+    if type(lista[0]) != dict:
+        return None
+    
+    for dicio in lista:
+        for nome  in list(dicio.keys()):    
+            if nome not in nomes:
+                nomes += [nome]
+                nomes += list(dicio.keys())   
+                agenda = dict.fromkeys(nomes)
+    
+        for nome in agenda:
+            agenda[nome] = []
+
+        #abrindo o dicionário
+        for dicio in lista:
+            #Olhando um nome de cada vez
+             for nome in nomes: 
+                #verificando o nome no dicionário
+                if nome in dicio:
+                    #Caso o nome esteja, adicione ao lado
+                    numero_do_contato=dicio.get(nome)
+                    agenda[nome]+= [numero_do_contato]   	
+
+
+        return agenda       	
+      
+#contatos(lista)
+
+#2)
+def piano(notas: str) -> list:
+    
