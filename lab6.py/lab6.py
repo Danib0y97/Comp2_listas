8362be7f4a9ee7fc92b768fdc0f56c142f4f260e
Danib0y97 <66141645+Danib0y97@users.noreply.github.com>
2021-09-01 21:17:04 -0300

    lab6.py

--- lab6.py/lab6.py
+++ lab6.py/lab6.py
@@ -1,4 +1,7 @@
-def escreverNoArquivo(nome_arq,lista):
+#Daniel Ferreira
+#1)
+def escreverNoArquivo(nome_arq:str,lista:list)-> str:
+    """Uma função onde, se escreve determinados elementos dentro de uma lista e retorna a quantidade de caracteres. Também, os elementos dessa lista criada ficam um em cima do outro."""
     nome_arq = open("arq.txt",'r+')
     string = str(lista)
     nome_arq.writelines(lista)
