5ac9c2ace23c9551d8b5f0c6b45fc718d6264598
Danib0y97 <66141645+Danib0y97@users.noreply.github.com>
2021-09-08 22:54:48 -0300

    update do lab6
    Ultima questão quase resolvida.

--- lab6.py/lab6.py
+++ lab6.py/lab6.py
@@ -23,41 +23,20 @@ def retornarLista(strlist :str)-> list:
         
 
 #3)
-def lerArquivo(nome_arq):
+def lerArquivo(nome_arq:str,lista:list) -> list:
     nome_arq = open("arq.txt",'r')
-
-#criar uma lista com elemento (linhas)
-#tirar o \n
-#converter lista p/ float, string, lista
-
-def f(nomeArq):
-
-#Abrir arquivo pra leitura
-# "-3.5\n9\n[12.0, '@9#', 9.8]\nab EE c\nk\n"
-    arquivo=open(nome)
-    
-#criar uma lista com cada elemento(linhas)
-#['-3.5\n', '9\n', "[12.0, '@9#', 9.8]\n",'ab EE c\n', 'k\n']
-    
-#tirar o \n
-#['-3.5', '9','ab EE c', "[12.0, '@9#', 9.8]",, 'k']
-    
-#convertendo string p/ float ,string, lista
-for indice in range(len(lista)):
-    try:
-        #converter pra número
-        listalimpa[indice]=float(listalimpa[indice])
-    except:
-        #tirar as asp
- #tirar as aspas
-        listalimpa[indice]=str(listalimpa[indice])
-
-        if "[" in listalimpa[indice]:
-        #converter lista
-            listalimpa[indice]=retornarLista(listalimpa[indice])
+    nome_arq.writelines(lista)
+    listalimpa = nome_arq.strip("\n")
+    for indice in range(len(lista)):
+        try:
+         listalimpa[indice]=float(listalimpa[indice])
+        except:
+            listalimpa[indice]=str(listalimpa[indice])
+            if "[" in listalimpa[indice]:
+              listalimpa[indice]= retornarLista(listalimpa[indice])
         
 
-return listalimpa
+    return listalimpa
 
 
       
