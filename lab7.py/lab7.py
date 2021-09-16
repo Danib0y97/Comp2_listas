49c65093bd5ad8ff39ede23e675e8ac9af0c2943
Danib0y97 <66141645+Danib0y97@users.noreply.github.com>
2021-09-15 22:48:16 -0300

    Update7.py
    Questão 2  finalizada

--- lab7.py/lab7.py
+++ lab7.py/lab7.py
@@ -2,17 +2,21 @@
 import numpy as np
 
 #1)
-def senoPositivo(a:int , b:int , n:int):
+def senoPositivo(a:int , b:int , n:int) -> np.vectorize:
+    '''função consiste em devolver os números onde o seno deles são positivos.'''
     c = np.linspace(a , b , n)
     d = np.sin(c) > 0
     return c[d]
     
 #2)
-def polinomio(vetor, z):
- 
-    return sum(np.cumprod(vetor)*z)
+def polinomio(vetor: np.vectorize, z:int) -> int:
+    '''A função recebe um vetor e um número nisso, ela retorna a soma de um polinomio, com eles sendo multiplicado pela váriavel Z e elevados pelo a posição começando em 0'''
+    a = np.comprod(z * np.ones(len(vetor)-1))
+    b = np.linspace(1 ,len(vetor)-1 ,len(vetor)-1 )
+    c = np.cumsum(vetor[b] * a)
+    return c.max() + vetor[0]
 
-coeficiente N * z **(elevado a N)
+
 
 
 
