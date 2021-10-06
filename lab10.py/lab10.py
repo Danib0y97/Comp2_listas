4fc34420be81e4f3a325340c3b4853ac455a7b13
Danib0y97 <66141645+Danib0y97@users.noreply.github.com>
2021-10-06 17:09:16 -0300

    Update do lab10.py
    Todas as questões feitas.

--- lab10.py/lab10.py
+++ lab10.py/lab10.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 #Questão 1
 def suites() -> 'series':
-    """"""
+    """Essa função conta a frequencia das suites e retorna o valor em Series. Também, faz um gráfico de pizza mostrando os valores das mesmas."""
  df = pd.read_csv("dados.csv")
  freq = df["suites"].value_counts()
   
@@ -22,7 +22,7 @@ def suites() -> 'series':
 
 #Questão 2
 def area() -> 'DataFrame':
-    """"""
+    """Mostra as áreas dos apartamentos em um histograma e o seu valor de retorno possui as linhas com as maiores áreas de todos os apartamentos."""
  df = pd.read_csv("dados.csv")
  df.groupby("area").mean()
  
@@ -34,3 +34,18 @@ def area() -> 'DataFrame':
  
  plt.show()
  return area_max
+
+ #Questão 3 
+def procura(preco, area, condominio) -> 'DataFrame':
+    """O usuário insere um valor para o preco, area e condominio. Nisso a função devolve os parametros, o preco menor ou igual do que o preco passado, a área maior ou igual e o condominio menor ou igual, distribuidos na coluna dos bairros. Além disso, ela cria um gráfico de barras mostrando os bairros."""
+  df = pd.read_csv("dados.csv")
+  
+  opcoes = df[(df["preco"] <= preco) & (df["area"] >= area) & (df["condominio"] <= condominio)]
+  
+  r = opcoes["bairro"].value_counts()
+
+  fig, axes = plt.subplots()
+  opcoes["bairro"].value_counts().plot.bar(rot = 0, color='orange')
+  
+  plt.show()
+  return r
